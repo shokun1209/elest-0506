8a02e2b55105c5ef9484bd359cf5a4cf2a9564ab
shokun1209 <shoya@satoushouyanoMacBook-Air.local>
2021-05-06 19:14:22 +0900

    Userモデル作成,usersテーブル作成

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,23 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  if Rails.env.development?
+    storage :file
+  elsif Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  # 受け付ける画像の種類
+  def extension_whitelist
+    %w(png jpg jpeg)
+  end
+
+  # ファイル名をランダムへ
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+end
