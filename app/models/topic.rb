9c67e04a9b49891137e5125dd5e81b8fc1db15d8
shokun1209 <shoya@satoushouyanoMacBook-Air.local>
2021-05-14 13:49:15 +0900

    複数の画像投稿機能実装

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,6 +2,7 @@ class Topic < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   mount_uploaders :images, ImageUploader
+  serialize :images, JSON
   belongs_to :user
 
   with_options presence: true do
@@ -9,4 +10,5 @@ class Topic < ApplicationRecord
     validates :text, length: { maximum: 1000 }
     validates :category_id, numericality: { other_than: 1 }
   end
+  validates :images, length:{ maximum: 4 }
 end
