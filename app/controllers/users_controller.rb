223caaaf49e96ba4754625898d812b16db3b43c2
shokun1209 <shoya@satoushouyanoMacBook-Air.local>
2021-07-08 17:18:12 +0900

    環境変数テスト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
-    @user.image.build
+    # @user.image.build
   end
   def show
     @user = User.find(params[:id])
