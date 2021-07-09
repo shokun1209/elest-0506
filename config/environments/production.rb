31fca36548b1591f8dfd29fab87c21063b617b87
shokun1209 <shoya@satoushouyanoMacBook-Air.local>
2021-07-09 17:47:25 +0900

    mailer URL修正 View修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,7 +111,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'http://elest.com' }
+  config.action_mailer.default_url_options = { host: 'http://elest0506.com' }
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
     :address => "smtp.gmail.com",
